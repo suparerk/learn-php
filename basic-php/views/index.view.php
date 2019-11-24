c9b9350c8f3006c308e2bf196a2153110a9966a6
Suparerk <ch.suparerk@gmail.com>
2019-11-24 15:02:44 +0700

    Form submit to db - Composer Autoloading

--- basic-php/views/index.view.php
+++ basic-php/views/index.view.php
@@ -1,16 +1,11 @@
 <?php require('partials/head.php'); ?>
-    <h1>Task for the day</h1>
-    <ul>
-      <?php foreach ($tasks as $task) : ?>
-        <li>
-          <?php if($task->completed) : ?>
-            <strike>
-              <?= $task->description; ?>
-            </strike>
-          <?php else : ?> 
-              <?= $task->description; ?>
-          <?php endif; ?> 
-        </li>
-      <?php endforeach; ?>
-    </ul>
+    <?php foreach ($users as $user) : ?>
+      <li><?= $user->name ?> </li>
+    <?php endforeach; ?>
+
+    <h1>Submit your name</h1>
+    <form method="POST" action="/names"> 
+      <input name="name"></input>
+      <button type="submit">Submit</button>
+    </form>
 <?php require('partials/footer.php'); ?>
